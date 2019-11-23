db2dae321abbba1545189f1c762ff9b0a4ebb014
Stuart Kyawmyint <kyawmyis@oregonstate.edu>
2019-11-22 19:12:50 -0800

    Copied original dominion files

---
+++ projects/FinalProject-Bugs/cmake-build-debug/CMakeFiles/dominion.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/dominion.c" "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/cmake-build-debug/CMakeFiles/dominion.dir/dominion.c.o"
+  "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/interface.c" "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/cmake-build-debug/CMakeFiles/dominion.dir/interface.c.o"
+  "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/playdom.c" "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/cmake-build-debug/CMakeFiles/dominion.dir/playdom.c.o"
+  "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/player.c" "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/cmake-build-debug/CMakeFiles/dominion.dir/player.c.o"
+  "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/rngs.c" "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/cmake-build-debug/CMakeFiles/dominion.dir/rngs.c.o"
+  "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/rt.c" "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/cmake-build-debug/CMakeFiles/dominion.dir/rt.c.o"
+  "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/testdrawcard.c" "/Users/stuartkyawmyint/Documents/OSU/CS362-F2019/dominion/cmake-build-debug/CMakeFiles/dominion.dir/testdrawcard.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
